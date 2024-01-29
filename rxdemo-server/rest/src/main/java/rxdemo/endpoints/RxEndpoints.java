bb28f2e097dfc6b8513bde77a45b06126689369a
DualDier <100816217+DualDier@users.noreply.github.com>
2024-01-29 10:40:12 -0600

    Update RxEndpoints.java

--- rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxEndpoints.java
+++ rxdemo-server/rest/src/main/java/rxdemo/endpoints/RxEndpoints.java
@@ -11,7 +11,7 @@ import spark.Response;
 import static spark.Spark.*;
 
 // change to be deployed
-// triggering a build2
+// triggering a build3
 
 public class RxEndpoints {
    public static void initalizeEndpoints() {
